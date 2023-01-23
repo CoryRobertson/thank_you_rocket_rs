7dafa5f0571097a3288220d5b7775893515c20e5
coryrobertson <cory.robertson17@gmail.com>
2023-01-22 19:57:15 -0800

    Program now saves the message output to a file.

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,140 @@
 #[macro_use]
 extern crate rocket;
+use chrono::{DateTime, Datelike, Local, Timelike};
 use rocket::form::Form;
 use rocket::response::content::RawHtml;
 use rocket::response::Redirect;
 use rocket::{Build, Rocket, State};
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
 use std::net::SocketAddr;
-use std::sync::{Arc, Mutex};
-use std::time::{SystemTime};
-use chrono::DateTime;
-use chrono_tz::Tz;
+use std::sync::{Arc, Mutex, MutexGuard};
+use std::thread::sleep;
+use std::time::{Duration, SystemTime};
+use std::{fs, thread};
 
 pub static POST_COOLDOWN: u64 = 5;
+pub static MESSAGE_LENGTH_CAP: usize = 150;
 
+#[derive(Clone)]
 struct Messages {
     // hash map consists of the ip address of the user: a vector of the messages they have left
     messages: Arc<Mutex<HashMap<String, User>>>,
 }
 
-// struct Message {
-//     text: String,
-//     time_stamp: DateTime<Tz>
-// }
+fn save_messages(messages: MutexGuard<HashMap<String, User>>) {
+    if !messages.is_empty() {
+        match fs::read_dir("./output") {
+            Ok(_) => {
+                // output dir exists
+            }
+            Err(_) => {
+                match fs::create_dir("./output") {
+                    Ok(_) => {
+                        // output dir now exists
+                    }
+                    Err(err) => {
+                        panic!(
+                            "{} \n unable to create output dir, check file permissions?",
+                            err
+                        )
+                    }
+                }
+            }
+        }
+        let file_name = {
+            // maybe use this later, at the moment not sure about it.
+            // let date_for_file_name = {
+            //     let ts = Local::now();
+            //     format!(
+            //         "{}-{}-{}",
+            //         ts.year(),
+            //         ts.month(),
+            //         ts.day(),
+            //     )
+            // }; // get a new date and time stamp for the file to save to
 
+            format!("./output/messages.sav")
+        };
+
+        let mut file = File::create(file_name).unwrap();
+        for (ip, user) in messages.iter() {
+            let messages = &user.messages;
+            let _ = file.write(format!("{ip}:\n").as_bytes()).unwrap();
+            for msg in messages {
+                let date = msg.time_stamp;
+                let am_pm = match date.hour12().0 {
+                    true => "PM",
+                    false => "AM",
+                };
+                let time_format = format!(
+                    "{}:{:02}:{:02}{}",
+                    date.hour12().1,
+                    date.minute(),
+                    date.second(),
+                    am_pm
+                );
+                let time_stamp_text = format!(
+                    "{}-{}-{}: {}",
+                    date.year(),
+                    date.month(),
+                    date.day(),
+                    time_format,
+                );
+                let _ = file
+                    .write(format!("\t[ {} ]: {}\n", time_stamp_text, msg.text).as_bytes())
+                    .unwrap();
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Message {
+    text: String,
+    time_stamp: DateTime<Local>,
+}
+
+#[derive(Debug)]
 struct User {
-    messages: Vec<String>,
+    messages: Vec<Message>,
     last_time_post: SystemTime,
 }
 
 impl Default for User {
     fn default() -> Self {
-        User { messages: vec![], last_time_post: SystemTime::now() }
+        User {
+            messages: vec![],
+            last_time_post: SystemTime::now(),
+        }
     }
 }
 
 impl User {
     /// Create a new user from a list of messages, time of last post established
-    fn new(message_vec: Vec<String>) -> User {
-        User { messages: message_vec, last_time_post: SystemTime::now() }
+    fn new(message: Message) -> User {
+        User {
+            messages: vec![message],
+            last_time_post: SystemTime::now(),
+        }
     }
     /// Add a new message to a user, and update their last time of posting
-    fn push(&mut self,msg: String) {
-        self.messages.push(msg);
+    fn push(&mut self, msg: String) {
+        let time = Local::now();
+        let message: Message = Message {
+            text: msg,
+            time_stamp: time,
+        };
+        self.messages.push(message);
         self.last_time_post = SystemTime::now();
     }
     /// Returns true if the user can post, and false if the user can not post.
     fn can_post(&self) -> bool {
         return match SystemTime::now().duration_since(self.last_time_post) {
-            Ok(dur) => {
-                dur.as_secs() >= POST_COOLDOWN
-            }
-            Err(_) => {
-                false
-            }
-        }
+            Ok(dur) => dur.as_secs() >= POST_COOLDOWN,
+            Err(_) => false,
+        };
     }
 }
 
@@ -69,11 +150,14 @@ impl Default for Messages {
 fn view(req: SocketAddr, messages: &State<Messages>) -> String {
     let user_ip = &req.ip().to_string();
     let msg_vec = match messages.messages.lock().unwrap().get(user_ip) {
-        None => { vec![] }
-        Some(user) => { user.messages.clone() }
+        None => {
+            vec![]
+        }
+        Some(user) => user.messages.clone(),
     };
+    let text_vec: Vec<String> = msg_vec.into_iter().map(|msg| msg.text).collect();
 
-    format!("{:?}", msg_vec).to_string()
+    format!("{:?}", text_vec).to_string()
 }
 
 #[get("/")]
@@ -82,7 +166,7 @@ fn index() -> RawHtml<&'static str> {
     <!DOCTYPE html>
         <html>
           <head>
-            <title>Title of the document</title>
+            <title>Thank_you_rocker_rs</title>
           </head>
           <body>
             <button onclick="window.location.href='/new';">
@@ -132,32 +216,55 @@ fn submit_message(
     req: SocketAddr,
     messages: &State<Messages>,
 ) -> Redirect {
-    println!("{:#?}", message.msg);
+    if !message.msg.is_ascii() {
+        return Redirect::to(uri!("/error_message")); // only allow user to use ascii text in their message
+    }
+
+    if message.msg.len() >= MESSAGE_LENGTH_CAP {
+        return Redirect::to(uri!("/too_long")); // early return and tell the user to write shorter messages
+    }
+
     let mut lock = messages.messages.lock().unwrap();
     let user_ip = &req.ip().to_string();
     match lock.get_mut(user_ip) {
         None => {
-            let mut new_vec = vec![]; // create a new vector and add it to this users ip address
-            new_vec.push(message.msg.to_string()); // eventually push the message they sent, not just underscores
-            lock.insert(user_ip.to_string(), User::new(new_vec)); // insert the new vector with the key of the users ip address
+            // let mut new_vec = vec![]; // create a new vector and add it to this users ip address
+            // new_vec.push(message.msg.to_string()); // eventually push the message they sent, not just underscores
+            let msg = Message {
+                text: message.msg.to_string(),
+                time_stamp: Local::now(),
+            };
+            lock.insert(user_ip.to_string(), User::new(msg)); // insert the new vector with the key of the users ip address
         }
         Some(user) => {
             // let time_since_last_post = SystemTime::now().duration_since(user.last_time_post).unwrap().as_secs();
-            if user.can_post() { // if the last time the user posted was 5 or more seconds ago
+            if user.can_post() {
+                // if the last time the user posted was 5 or more seconds ago
                 user.push(message.msg.to_string()); // push their new message, this also updates their last time of posting
-                // user.last_time_post = SystemTime::now(); // update their last post time
+                                                    // user.last_time_post = SystemTime::now(); // update their last post time
             } else {
                 user.last_time_post = SystemTime::now();
                 return Redirect::to(uri!("/slow_down")); // early return and tell the user to slow down
             }
         }
     };
+
     Redirect::to(uri!("/"))
 }
 
 #[get("/slow_down")]
-fn slow_down(_req: SocketAddr, _messages: &State<Messages>) -> String {
+fn slow_down() -> String {
     format!("Please slow down, you are trying to post too often :)")
+}
+
+#[get("/too_long")]
+fn too_long() -> String {
+    format!("That message is too long, please try to make it shorter :)")
+}
+
+#[get("/error_message")]
+fn error_message() -> String {
+    format!("That message for some reason was unable to be saved (most likely contains something that is not ascii). ¯\\_(ツ)_/¯")
 }
 
 #[get("/submit_message")]
@@ -168,8 +275,28 @@ fn submit_message_no_data() -> Redirect {
 #[launch]
 fn rocket() -> Rocket<Build> {
     // using this return type isn't shown in the documentation from my minimal looking, but makes intellij happy.
-    rocket::build().manage(Messages::default()).mount(
+    let state = Messages::default();
+    let message_reference = Arc::clone(&state.messages);
+
+    thread::spawn(move || loop {
+        {
+            let lock = message_reference.lock().unwrap();
+            save_messages(lock);
+        }
+        sleep(Duration::from_secs(5));
+    }); // file save loop
+
+    rocket::build().manage(state).mount(
         "/",
-        routes![index, submit_message, new, submit_message_no_data, view,slow_down],
+        routes![
+            index,
+            submit_message,
+            new,
+            submit_message_no_data,
+            view,
+            slow_down,
+            too_long,
+            error_message
+        ],
     )
 }
